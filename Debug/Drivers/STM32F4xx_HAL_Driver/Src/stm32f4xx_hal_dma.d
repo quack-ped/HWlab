153da871f48efb012a65976af640c27659401301
quack-ped <quack.ped@gmail.com>
2017-12-21 13:08:12 +0700

    Add files via upload
    File for stm32

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.d
@@ -0,0 +1,80 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Inc/main.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Inc/stm32f4xx_hal_conf.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Inc/main.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Users/dreamy/Desktop/HW/Project\ -\ Copy/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
